e2ea27582d62d9a96d70240182c8e8cd07edb334
Miguel Franco <francamente@hotmail.es>
2023-11-17 21:13:03 +0100

    correccion apiBook

--- http-express-y-api-rest/apiBook/src/controller/book.controller.js
+++ http-express-y-api-rest/apiBook/src/controller/book.controller.js
@@ -21,7 +21,7 @@ function getBook(req, res) {
   if (libro) {
     respuesta = { error: false, codigo: 200, data: libro };
   } else {
-    respuesta = { error: true, codigo: 200, data: "El libro no existe" };
+    respuesta = { error: true, codigo: 200, data: "El libro es null" };
   }
   res.send(respuesta);
 }
@@ -29,15 +29,14 @@ function getBook(req, res) {
 function postBook(req, res) {
   let respuesta;
   if (!libro) {
-    libro = new Book(
-      "Crimen y castigo",
-      "Novela",
-      "Fiodor Dostoyevski",
-      30,
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7b/Crime_and_Punishment-1.png/375px-Crime_and_Punishment-1.png",
-      1,
-      1
-    );
+    libro = {};
+    libro.title = req.body.title;
+    libro.type = req.body.type;
+    libro.author = req.body.author;
+    libro.price = req.body.price;
+    libro.photo = req.body.photo;
+    libro.id_book = req.body.id_book;
+    libro.id_user = req.body.id_user;
     respuesta = {
       error: false,
       codigo: 200,
@@ -53,14 +52,13 @@ function postBook(req, res) {
 function putBook(req, res) {
   let respuesta;
   if (libro) {
-    libro.title = "Momo";
-    libro.type = "Novela";
-    libro.author = "Michael Ende";
-    libro.price = "25";
-    libro.photo =
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4a/Momo_Figuur_Hannover.jpg/330px-Momo_Figuur_Hannover.jpg";
-    libro.id_book = "1";
-    libro.id_user = "1";
+    libro.title = req.body.title;
+    libro.type = req.body.type;
+    libro.author = req.body.author;
+    libro.price = req.body.price;
+    libro.photo = req.body.photo;
+    libro.id_book = req.body.id_book;
+    libro.id_user = req.body.id_user;
     respuesta = {
       error: false,
       codigo: 200,
@@ -68,7 +66,11 @@ function putBook(req, res) {
       data: libro,
     };
   } else {
-    respuesta = { error: true, codigo: 200, mensaje: "No hay ningun libro" };
+    respuesta = {
+      error: true,
+      codigo: 200,
+      mensaje: "No hay ningun libro que editar",
+    };
   }
   res.send(respuesta);
 }
